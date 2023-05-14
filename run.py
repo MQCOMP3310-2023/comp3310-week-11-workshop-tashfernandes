3006c8324c9f9fe5c066a23d5693b04bf69c4844
Natasha Fernandes <tashfernandes@gmail.com>
2023-05-14 17:56:51 +1000

    Whitespace

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from project import create_app
 
 # Adding some whitespace
-
+# adding some more stuff in dev
 if __name__ == '__main__':
   app = create_app()
   app.run(host = '0.0.0.0', port = 8001, debug=True) 
